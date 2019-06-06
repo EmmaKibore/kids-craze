140ecf02296b98f7f9e89b4323f786b29439ceb7
EmmaKibore <emmakibore@gmail.com>
2019-06-06 11:40:35 +0300

    added location models

--- craze/models.py
+++ craze/models.py
@@ -1,14 +1,11 @@
 from django.db import models
 
-class Profile
-
-ser = models.ForeignKey(User, on_delete=models.CASCADE,related_name='profile')
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE,related_name = 'profile')
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    bio = models.CharField(max_length=100)
     profile_pic = models.ImageField(upload_to='profile/')
-    pub_date_created = models.DateTimeField(auto_now_add=True, null=True)
-    neighbourhood = models.ForeignKey('Neighbourhood', blank=True, null=True)
+
 
     def __str__(self):
         return self.first_name
@@ -36,14 +33,14 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
-        class Category(models.Model):
-    name = models.CharField(max_length=30)
+    #     class Category(models.Model):
+    # name = models.CharField(max_length=30)
 
-    def save_category(self):
-        self.save()
+    # def save_category(self):
+    #     self.save()
 
-    def delete_category(self):
-        self.delete()
+    # def delete_category(self):
+    #     self.delete()
 
-    def __str__(self):
-        return self.name
+    # def __str__(self):
+    #     return self.name
