962f3628d6789046472b448e80cc571bf429cc1b
EmmaKibore <emmakibore@gmail.com>
2019-06-06 12:40:49 +0300

    specified home and profile urls

--- craze/urls.py
+++ craze/urls.py
@@ -2,3 +2,13 @@ from django.conf.urls import url, include
 from . import views
 from django.conf import settings 
 from django.conf.urls.static import static
+
+urlpatterns=[
+    url('^$',views.home,name = 'home'),
+    url(r'^accounts/',include('registration.backends.simple.urls')),
+    url(r'profile/$',views.profile,name='profile'),
+    url(r'^search/', views.search_results, name='search_results'),
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
