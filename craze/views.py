acc8e9c169302750164df5dc5df3c995e832b0fd
EmmaKibore <emmakibore@gmail.com>
2019-06-06 12:46:40 +0300

    imported forms in views

--- craze/views.py
+++ craze/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
+from .models import *
+from .forms import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
