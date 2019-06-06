5f167f4aab558c2292222177e2e0835f03b9abe6
EmmaKibore <emmakibore@gmail.com>
2019-06-06 11:31:47 +0300

    added Profile model

---
+++ craze/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+class Profile
+
+ser = models.ForeignKey(User, on_delete=models.CASCADE,related_name='profile')
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    bio = models.CharField(max_length=100)
+    profile_pic = models.ImageField(upload_to='profile/')
+    pub_date_created = models.DateTimeField(auto_now_add=True, null=True)
+    neighbourhood = models.ForeignKey('Neighbourhood', blank=True, null=True)
+
+    def __str__(self):
+        return self.first_name
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_profiles(cls):
+        profiles = cls.objects.all()
+        return profiles
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
+
+        class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def __str__(self):
+        return self.name
