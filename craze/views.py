0915c510d98844e77c9fa543087e7606f6c424a6
EmmaKibore <emmakibore@gmail.com>
2019-06-06 12:53:49 +0300

    created profile view function

--- craze/views.py
+++ craze/views.py
@@ -5,4 +5,27 @@ from .forms import *
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
-# Create your views here.
+def home(request):
+
+    return render(request, 'home.html')
+
+def profile(request):
+    current_user = request.User
+    profile = Profile.objects.all()
+
+    return render(request, 'profile.html', {"profile":profile}) 
+
+def search_results(request):
+    if 'business' in request.GET and request.GET["business"]:
+        search_term = request.GET.get("business")
+        searched_businesses = Business.objects.filter(name=search_term)
+        message = f"{search_term}"
+        profiles=  Profile.objects.all( )
+      
+        return render(request, 'search.html',{"message":message,"business": searched_businesses,'profiles':profiles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
+
+
